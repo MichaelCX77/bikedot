3493ed444e82fd108fcb2764fece3d06e4b81137
Michael Xavier <maycow_77@hotmail.com>
2021-11-09 12:37:13 -0300

    Add server eureka

---
+++ bikedot-eureka-server/src/test/java/com/mcx/bikedotgateway/BikedotEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcx.bikedotgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikedotEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
