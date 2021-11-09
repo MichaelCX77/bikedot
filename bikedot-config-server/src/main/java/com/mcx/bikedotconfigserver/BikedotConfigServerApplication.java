e62d122e68908e546018a543cb47e55c6270f1bd
Michael Xavier <maycow_77@hotmail.com>
2021-11-09 13:13:03 -0300

    Add config server

---
+++ bikedot-config-server/src/main/java/com/mcx/bikedotconfigserver/BikedotConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.mcx.bikedotconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BikedotConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikedotConfigServerApplication.class, args);
+	}
+
+}
---
+++ bikedot-config-server/src/test/java/com/mcx/bikedotconfigserver/BikedotConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcx.bikedotconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikedotConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
